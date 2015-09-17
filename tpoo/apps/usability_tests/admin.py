439108fc1039a8364eb596f2bd39c7eb3abe424f
Laura Lus <mlaura.lus@gmail.com>
2015-09-17 11:47:50 -0300

    admin site

--- tpoo/apps/usability_tests/admin.py
+++ tpoo/apps/usability_tests/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import UsabilityTest, Task, Refactoring, Scenario, AppVersion
 
-# Register your models here.
+
+admin.site.register(UsabilityTest)
+admin.site.register(Task)
+admin.site.register(Refactoring)
+admin.site.register(Scenario)
+admin.site.register(AppVersion)
+
+
