9ddd2ab150a04b3fd7475eb0b2cc41e8f07c974a
Matías Celasco <matiascelasco@gmail.com>
2015-12-02 20:42:25 -0300

    comment

--- tpoo/utils/views.py
+++ tpoo/utils/views.py
@@ -2,6 +2,7 @@ from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 
 
+# IMPORTANT: this mixin should be put first in the inheritance chain
 class LoginRequiredMixin(object):
     u"""Ensures that user must be authenticated in order to access view."""
 
