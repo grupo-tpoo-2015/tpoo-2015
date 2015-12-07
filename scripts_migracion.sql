ddd5db2ce97410bfd129f11b67a237ab2eb87e22
Matías Celasco <matiascelasco@gmail.com>
2015-12-07 19:09:32 -0300

    Observation type names

--- scripts_migracion.sql
+++ scripts_migracion.sql
@@ -9,7 +9,7 @@ select id, name
 from tpoodump.refactorings;
 
 /*migra usability tests*/
-SET @idSuperUser = 
+SET @idSuperUser =
 (select min(id)
  from auth_user
  where is_superuser);
@@ -39,7 +39,7 @@ from tpoodump.task;
 insert into usability_tests_scenario(id, name, app_version_id)
 select S.id, S.name, AV.id
 from tpoodump.scenario S inner join usability_tests_appversion AV on (S.usability_test_id = AV.usability_test_id)
-where (S.name like '%Non Refactored' and AV.name ='Non Refactored') or 
+where (S.name like '%Non Refactored' and AV.name ='Non Refactored') or
 	  (S.name not like '%Non Refactored' and AV.name ='Refactored');
 
 /*migra scenario_task, lo que antes era task_for_version*/
@@ -52,8 +52,8 @@ insert into tasks_interactionstep(id, scenario_task_id, is_question, name, `orde
 select id, task_version_id, question, name, step_order
 from tpoodump.step S;
 
-/*relacion entre scenario_task y refactoring -many to many, TODO en el modelo 
-la relacion entre refactoring y appVersion vuela. 
+/*relacion entre scenario_task y refactoring -many to many, TODO en el modelo
+la relacion entre refactoring y appVersion vuela.
 Se reemplaza por metodo que hace las queries necesarias
 */
 insert into tasks_scenariotask_refactorings(scenariotask_id, refactoring_id)
@@ -93,19 +93,19 @@ from tpoodump.step_execution SE inner join tpoodump.step S on (SE.step_id = S.id
 
 /*plantea la observación tiempo*/
 insert into tasks_observationtype(id, name, unit)
-values (1,'time', 'seconds');
+values (1,'Tiempo', 'seconds');
 
 /*plantea la observación demográficas*/
 insert into tasks_observationtype(id, name, unit)
-values (2,'dem', 'free');
+values (2,'Preguntas demográficas', 'free');
 
 /*plantea la observación satisfacción*/
 insert into tasks_observationtype(id, name, unit)
-values (3,'satisfaction', '1..5');
+values (3,'Preguntas de satisfacción', '1..5');
 
 /*guarda el observation tiempo para step que no son preguntas*/
 insert into tasks_interactionstep_observation_types(interactionstep_id, observationtype_id)
-select id, 1 /*el tiempo sólo a los que no son question*/ 
+select id, 1 /*el tiempo sólo a los que no son question*/
 from tpoodump.step
 where not question;
 
@@ -113,7 +113,7 @@ where not question;
 insert into tasks_interactionstep_observation_types(interactionstep_id, observationtype_id)
 select id, 2
 from tpoodump.step
-where question 
+where question
   and ((name like 'Ingrese su edad:')
   or  (name like 'Ha trabajado o trabaja en:')
   or  (name like 'FormaciÃ³n acadÃ©mica:'));
@@ -122,23 +122,23 @@ where question
 insert into tasks_interactionstep_observation_types(interactionstep_id, observationtype_id)
 select distinct(S.id), 3
 from tpoodump.step S inner join tpoodump.step_execution SE on (S.id = SE.step_id)
-where question 
+where question
   and not((name like 'Ingrese su edad:')
   or  (name like 'Ha trabajado o trabaja en:')
   or  (name like 'FormaciÃ³n acadÃ©mica:'))
 and (length(SE.result)= 1);
 
-insert into usability_tests_executions_observation(`value`, observation_type_id, step_execution_id) 
+insert into usability_tests_executions_observation(`value`, observation_type_id, step_execution_id)
 /*esta consulta agrega el observation_type time */
 select SE.time, 1, SE.id
 from tpoodump.step S inner join tpoodump.step_execution SE on (S.id = SE.step_id)
 where not question;
 
-insert into usability_tests_executions_observation(`value`, observation_type_id, step_execution_id) 
+insert into usability_tests_executions_observation(`value`, observation_type_id, step_execution_id)
 /*esta consulta agrega el observation_type satisfaction */
 select cast(SE.result AS DECIMAL(10,2)), 3, SE.id
 from tpoodump.step S inner join tpoodump.step_execution SE on (S.id = SE.step_id)
-where question 
+where question
   and not((name like 'Ingrese su edad:')
   or  (name like 'Ha trabajado o trabaja en:')
   or  (name like 'FormaciÃ³n acadÃ©mica:'))
